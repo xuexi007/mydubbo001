fc9977dbadab6ae7c5dc5bafe2dd407cb9a9206c
sunny <test@qq.com>
2019-09-18 15:24:10 +0800

    新增注释

--- myConsumer/src/main/java/com/offcn/ConsumerStart.java
+++ myConsumer/src/main/java/com/offcn/ConsumerStart.java
@@ -3,6 +3,9 @@ package com.offcn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/***
+ * 增肌zhussdss
+ */
 @SpringBootApplication
 public class ConsumerStart {
     public static void main(String[] args) {
